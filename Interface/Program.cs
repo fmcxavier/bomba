c3f003db932917170c846dc06673ebfee954bc71
fmcxavier <fabiomcxavier@mail.com>
2019-08-27 23:18:08 +0100

    position command line and draw symbol

--- Interface/Program.cs
+++ Interface/Program.cs
@@ -35,7 +35,40 @@ namespace Interface
         private static void StartScreen()
         {
             Console.Clear();
-            Console.WriteLine("Choose what wires to cut in order separated with a comma:");
+            string line = "Choose what wires to cut in order separated with a comma:";
+
+            DrawSymbol();
+
+            var bottomLine = Console.WindowHeight;
+            Console.SetCursorPosition(0,bottomLine - 1);
+
+            Console.WriteLine(line);
         }
+
+        private static void DrawSymbol()
+        {
+            var symbolLength = _bombSymbol[0].Length;
+            _symbolLeftOffset = (Console.WindowWidth - symbolLength) / 2;
+            _symbolTopOffset = (Console.WindowHeight - _bombSymbol.Length - 2) / 2;
+
+
+            var lineCount = 0;
+            foreach (var line in _bombSymbol)
+            {
+                Console.SetCursorPosition(_symbolLeftOffset, _symbolTopOffset + lineCount);
+                Console.WriteLine(line);
+                ++lineCount;
+            }
+        }
+
+        private static int _symbolLeftOffset;
+        private static int _symbolTopOffset;
+        private static string[] _bombSymbol = new [] {
+            "                                                         c=====e" ,
+            "                                                            H   " ,
+            "   ____________                                         _,,_H_ _" ,
+            "  (__((__((___()                                       //|     |" ,
+            " (__((__((___()()_____________________________________// |ACME |" ,
+            "(__((__((___()()()------------------------------------'  |_____|" } ;
     }
 }
